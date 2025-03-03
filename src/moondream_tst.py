b22806719c8e6b1db39ef22bb6606c90ebf2eb39
M. Johansson <MathiasJoha@gmail.com>
2025-03-03 10:50:40 +0100

    Process with moondream

--- src/moondream_tst.py
+++ src/moondream_tst.py
@@ -5,6 +5,9 @@ Download model: https://huggingface.co/vikhyatk/moondream2/resolve/9dddae84d54db
 import moondream as md
 from PIL import Image
 from trope_paths import moondream_model, output_dir
+from trope_paths import raw_dir, moon_tmp_dir, read_data
+from tqdm import tqdm
+import json
 
 # Initialize with local model path. Can also read .mf.gz files, but we recommend decompressing
 # up-front to avoid decompression overhead every time the model is initialized.
@@ -21,3 +24,47 @@ print("Caption:", caption)
 # Ask questions
 answer = model.query(encoded_image, "What's in this image?")["answer"]
 print("Answer:", answer)
+
+if __name__ == "__main__":
+
+    data = read_data()
+
+    dataw = data[data.apply(lambda x: x["gt_w"], axis=1)]
+    datanw = data[data.apply(lambda x: not x["gt_w"], axis=1)]
+
+    total = 20
+    each = int(total / 2)
+
+    file_names = set(
+        dataw["file_name"].to_list()[:each]
+        + datanw["file_name"].to_list()[:each]
+    )
+
+    transactions = []
+    for file_name in tqdm(file_names):
+        out_name = file_name + ".json"
+        output_file = moon_tmp_dir / out_name
+        if output_file.exists():
+            continue
+        source_path = raw_dir / file_name
+        transactions.append((source_path, output_file))
+
+    for image, output in tqdm(transactions):
+
+        imagef = Image.open(output_dir / "DinoWomanMan_th25/022yi14dr7qw.png")
+        encoded_image = model.encode_image(imagef)
+
+        # encoded_image = model.encode(Image.open(image))
+
+        men = model.point(encoded_image, "man")["points"]
+        women = model.point(encoded_image, "women")["points"]
+        people = model.point(encoded_image, "person")["points"]
+
+        content = {
+            "people": len(people),
+            "man": len(men),
+            "woman": len(women),
+        }
+
+        with open(output, "x", encoding="utf-8") as f:
+            f.write(json.dumps(content))
